af822e3a44df91c8648dc2c0c542ab4a5ced8821
Seb-Leb <seb@leb.cat>
2020-06-19 14:01:08 -0400

    added links to OpenProt

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+	$("#details_tree").jstree();
 	let myRequest = new Request('https://raw.githubusercontent.com/Seb-Leb/protein_clusters/master/clusters.json');
 	fetch(myRequest)
 		.then(function(res) {
@@ -7,7 +8,9 @@ $(document).ready(function() {
 		.then(function(clusters) {
 			clusters_graph = clusters.clusters_graph;
 			node_positions = clusters.node_positions;
-			cluster_det = clusters.clusters;
+			cluster_det    = clusters.clusters;
+
+
 
 			var cy = cytoscape({
 				container: document.getElementById('cy'),
@@ -27,26 +30,33 @@ $(document).ready(function() {
 				//positions: function(n) {return node_positions[n.data("name")]},
 			});
 			layout.run();
-			
+
+		    var h = function(tag, attrs, children){
+		      var el = document.createElement(tag);
+
+		      Object.keys(attrs).forEach(function(key){
+		        var val = attrs[key];
+
+		        el.setAttribute(key, val);
+		      });
+
+		      children.forEach(function(child){
+		        el.appendChild(child);
+		      });
+
+		      return el;
+		    };
+
+		    var t = function(text){
+		      var el = document.createTextNode(text);
+		      return el;
+		    };
+
+
+
 			cy.nodes().forEach(function(n){
 				n.on('click', function(){
 					clust_name = 'cluster_'.concat(n.data("name"));
-					var cy_clust = cytoscape({
-					container: document.getElementById('clust_cy'),
-					elements: cluster_det[clust_name].cytoscape.elements,
-					style: cytoscape.stylesheet()
-						.selector('node')
-							.style({
-								'label': function(ele){return ele.data("name")},
-								"text-valign": "center",
-								"text-halign": "center",
-							})
-					});
-					cy_clust.nodes('[tt_type = "alt"]').style('background-color', '#d81159');
-					cy_clust.nodes('[tt_type = "ref"]').style('background-color', '#2582c6');
-					var clust_layout = cy_clust.layout({name:'cose'});
-					clust_layout.run();
-
 					var details_tree = {
 										'core' : {
 											'data' : [
@@ -65,11 +75,78 @@ $(document).ready(function() {
 											]
 										}
 									};
-					$("#Gene_Ontology").jstree("destroy")
-					$("#Gene_Ontology").data('jstree', false).empty().jstree(details_tree);
-
-					$('#Gene_Ontology').on("select_node.jstree", function (e, data) {
+					$("#details_tree").jstree("destroy");
+					$("#details_tree").data('jstree', false).empty().jstree(details_tree);
+					$('#details_tree').on("select_node.jstree", function (e, data) {
 					  data.instance.toggle_node(data.node);
+					});
+					var cy_clust = cytoscape({
+						container: document.getElementById('clust_cy'),
+						elements: cluster_det[clust_name].cytoscape.elements,
+						style: cytoscape.stylesheet()
+							.selector('node')
+								.style({
+									'label': function(ele){return ele.data("name")},
+									"text-valign": "center",
+									"text-halign": "center",
+								})
+					});
+					var makeTippy = function(node, html){
+				      return tippy( node.popperRef(), {
+				        html: html,
+				        trigger: 'manual',
+				        theme: 'light',
+				        arrow: true,
+				        placement: 'bottom',
+				        hideOnClick: false,
+				        interactive: true
+				      } ).tooltips[0];
+				    };
+
+				    var hideTippy = function(node){
+					    var tippy = node.data('tippy');
+					      if(tippy != null){
+					        tippy.hide();
+					      }
+				    };
+
+				    var hideAllTippies = function(){
+				      cy_clust.nodes().forEach(hideTippy);
+				    };
+					cy_clust.nodes('[tt_type = "alt"]').style('background-color', '#d81159');
+					cy_clust.nodes('[tt_type = "ref"]').style('background-color', '#2582c6');
+					var clust_layout = cy_clust.layout({name:'cose'});
+					clust_layout.run();
+					cy_clust.on('tap', function(e){
+				      if(e.target === cy_clust){
+				        hideAllTippies();
+				      }
+				    });
+
+				    cy_clust.on('tap', 'edge', function(e){
+				      hideAllTippies();
+				    });
+
+				    cy_clust.on('zoom pan', function(e){
+				      hideAllTippies();
+				    });
+					cy_clust.nodes().forEach(function(n){
+						var $links = [
+						{
+							name : 'OpenProt',
+							url  : n.data('OP_link')
+						}
+						].map(function(link){
+							return h('a', {target:'_blank', href: link.url, 'class':'tip-link'}, [t(link.name)]);
+						});
+
+						var tippy = makeTippy(n, h('div', {}, $links))
+
+						n.data('tippy', tippy);
+						n.on('click', function(e){
+							tippy.show();
+							cy_clust.nodes().not(n).forEach(hideTippy);
+						});
 					});
 				});
 			});
